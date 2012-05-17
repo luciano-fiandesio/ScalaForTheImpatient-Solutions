af2d29362c76d1b73dfecadc96205f25ea3d6ad6
AestasIT <andrey@aestasit.com>
2012-05-17 21:34:29 +0300

    Added exercises for chapter 2, minus exercise 10

--- src/test/scala/Chapter2.scala
+++ src/test/scala/Chapter2.scala
@@ -22,4 +22,58 @@ class Chapter2 extends FunSuite {
         val empty = {}
         assert(isUnit(empty))
     }
+    
+    // 3. TODO
+
+    test("4. Write a scala equivalent for the Java loop...") {
+      	for (i <- 10 to 0 by -1) {
+			println (i)
+        }
+    }
+
+	test("5. Write a procedure countdown(n: Int) that prints the numbers from n to 0.") {
+		// No assertion here...
+		countdown(55)
+	}
+	
+	def countdown(upperLimit:Int) {
+		for (i <- upperLimit to 0 by -1) {
+			println (i)
+        }
+	}
+	
+	test("6. Write a for loop for computing the product of the Unicode codes of all letters in a string") {
+		var unicode:Long = 1
+		for (s <- "Hello") {
+			unicode *= s.toInt
+		}
+		assert(9415087488L == unicode)
+	}
+	
+	test("7. Solve the preceding exercise without writing a loop.") {
+		assert(9415087488L == "Hello".foldLeft(1L)(_*_))
+	}
+	
+	test("8. Write a function product(s : String) that computes the product") {
+		assert(9415087488L == product("Hello"))
+	}
+	
+	def product(s:String) = {
+		s.foldLeft(1L)(_*_)
+	}
+	
+	test("9. Make the function of the preceding exercise a recursive function.") {
+		assert(9415087488L == productR("Hello"))
+	}
+	
+	def productR(s:String):Long = {
+		if(s.length==0) 1 else s(0)*productR(s drop 1)
+	}
+	
+	test("10. Write a function that computes xn, where n is an integer") {
+	
+		// TODO
+	}
+	
+
 }
